5ac598059940780dffa44fd511b2379da2c96d3d
NtandoNokilana <ntandonokilana@gmail.com>
2024-03-07 16:33:30 +0200

    updated files

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,12 +1,15 @@
 <template>
-    <div>
-        
+    <div class="product-grid">
+      <div v-for="product in $store.state.products" :key="product.prodID" class="product-card">
+        <h2>{{ product.prodname }}</h2>
+        <p>Price: {{ product.category }}</p>
+        <p>Category: {{ product.amount }}</p>
+        <img :src="product.prodUrl" alt="Product Image" style="max-width: 100%; height: auto;">
+      </div>
     </div>
-</template>
+  </template>
 <script>
-export default {
-    
-}
+
 </script>
 <style>
     
