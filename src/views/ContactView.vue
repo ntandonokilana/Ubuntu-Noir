8aa006e62777eb1a9da49d1dbe163ee8b14a6f2c
unknown <ntandonokilana@gmail.com>
2024-03-24 17:01:24 +0200

    fixing contact-page

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,210 +1,131 @@
 <template>
-  <div class="product-page">
-    <div class="product-filter">
-      <input type="text" v-model="searchTerm" placeholder="Search products..." class="search-input">
-      <div class="sort-dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="sortDropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Sort
-        </button>
-        <div class="dropdown-menu" aria-labelledby="sortDropdownMenuButton">
-          <button class="dropdown-item" @click="sortBy('name')">Name A-Z</button>
+  <div class="contact-page">
+    <div class="background-image"></div>
+    <div class="contact-form">
+      <h2>Write Us!</h2>
+      <form action="https://formspree.io/f/xyyrpzdo" method="POST">
+        <div class="form-group">
+          <label for="fullname">Full Name:</label>
+          <input type="text" id="fullname" name="fullname" placeholder="Your Full Name" required>
         </div>
-      </div>
-    </div>
-    <div class="product-grid">
-      <div v-for="product in filteredProducts" :key="product.productID" class="product-card">
-        <h2 class="product-title">{{ product.prodname }}</h2>
-        <img :src="product.produrl" alt="Product Image" class="product-image">
-        <div class="product-details">
-          <p>Category: {{ product.category }}</p>
-          <p>Price: {{ product.amount }}</p>
-          <div class="product-buttons">
-            <button @click="addToCart(product)" class="btn btn-primary add-to-cart-btn" data-mdb-ripple-init>Add to Cart</button>
-            <!-- Modify the anchor tag to redirect to SinglePageView.vue -->
-            <router-link :to="{ name: 'SinglePageView', params: { productId: product.productID }}" class="btn btn-primary view-more-btn" data-mdb-ripple-init>View More</router-link>
+        <div class="form-group">
+          <label for="email">Email Address:</label>
+          <input type="email" id="email" name="email" placeholder="Your Email Address" required>
+        </div>
+        <div class="form-group">
+          <label for="message">Message:</label>
+          <textarea id="message" name="message" placeholder="Message Us" required></textarea>
+        </div>
+        <div class="send-message">
+          <button type="submit">Send</button>
+          <div class="contact-icons">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+              <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+            </svg>
+            <div class="contact-info">
+              <p style="color: #fff;">021 386-2367</p>
+              <p style="color: #fff;">info@ubuntunoir.co.za</p>
+            </div>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   </div>
 </template>
 
-<script>
-import { routerLink } from 'vue-router';
-import Swal from 'sweetalert2';
-
-export default {
-  data() {
-    return {
-      selectedCategory: 'all',
-      searchTerm: '',
-      sortByOption: '',
-    };
-  },
-  computed: {
-    products() {
-      return this.$store.state.products;
-    },
-    filteredProducts() {
-      let filtered = this.products;
-
-      if (this.selectedCategory !== 'all') {
-        filtered = filtered.filter(product => product.category === this.selectedCategory);
-      }
-
-      if (this.searchTerm.trim() !== '') {
-        const regex = new RegExp(this.searchTerm.trim(), 'i');
-        filtered = filtered.filter(product => regex.test(product.prodname));
-      }
-
-      if (this.sortByOption === 'name') {
-        filtered.sort((a, b) => a.prodname.localeCompare(b.prodname));
-      }
-
-      return filtered;
-    }
-  },
-  mounted() {
-    this.$store.dispatch('fetchProducts'); 
-  },
-  methods: {
-    async addToCart(product) {
-      try {
-        // Add product to cart logic 
-        await this.$store.dispatch('addToCart', product);
-
-        // Show success message using SweetAlert
-        Swal.fire({
-          icon: 'success',
-          title: 'Item Added to Cart!',
-          showConfirmButton: false,
-          timer: 1500
-        });
-      } catch (error) {
-        console.error('Error adding item to cart:', error);
-        // Show error message using SweetAlert if necessary
-        Swal.fire({
-          icon: 'error',
-          title: 'Oops...',
-          text: 'An error occurred while adding the item to the cart. Please try again later.',
-        });
-      }
-    },
-    sortBy(option) {
-      this.sortByOption = option;
-    }
-  }
-};
-</script>
-
 <style scoped>
-.product-page {
-  padding-top: 20px;
-  background-image: url('https://i.ibb.co/n6CCRrb/henry-co-AW-P-NU9-MI8-unsplash.jpg');
-}
-
-.product-filter {
-  margin-bottom: 80px;
-}
-
-.search-input {
-  padding: 8px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  margin-right: 10px;
-}
-
-.sort-dropdown {
+.contact-page {
   position: relative;
-  display: inline-block;
-}
-
-.sort-dropdown .dropdown-toggle {
-  background-color: #6f6848; 
-  color: #fff;
-  border-radius: 5px;
-  padding: 8px 16px;
-  border: none;
-  cursor: pointer;
-}
-
-.sort-dropdown .dropdown-toggle:hover {
-  background-color: #000000; 
-}
-
-.sort-dropdown .dropdown-menu {
-  position: absolute;
-  background-color: #fff;
-  min-width: 160px;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  z-index: 1;
-}
-
-.sort-dropdown .dropdown-item {
-  padding: 8px 16px;
-  cursor: pointer;
-}
-
-.sort-dropdown .dropdown-item:hover {
-  background-color: #f1f1f1;
-}
-
-.product-grid {
-  display: grid;
-  grid-template-columns: repeat(3, minmax(250px, 1fr));
-  gap: 30px;
-  margin: auto;
-}
-
-.product-card {
-  width: calc(50% - 20px); 
-  border: 1px solid #ccc;
-  padding: 10px; 
-  border-radius: 10px;
-  transition: transform 0.5s ease;
-  margin: auto;
-}
-
-.product-card:hover {
-  transform: translateY(-10px);
-}
-
-.product-image {
   width: 100%;
-  border-radius: 5px;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-.product-title {
-  font-size: 1.4rem;
-  margin-top: 0;
-  margin-bottom: 10px;
+.background-image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url('https://i.ibb.co/s2SRGkP/mohammad-dadkhah-h33si-B2533-Y-unsplash.jpg');
+  background-size: cover;
+  z-index: -1;
 }
 
-.product-details p {
-  font-size: 1rem;
-  margin: 5px 0;
+.contact-form {
+  max-width: 500px;
+  background: rgba(0, 0, 0, 0.5);
+  padding: 20px;
+  border-radius: 10px;
 }
 
-.product-buttons {
-  margin-top: 10px;
-}
-
-.add-to-cart-btn,
-.view-more-btn {
-  display: inline-block;
-  padding: 8px 16px;
-  background-color: #0056b3;
+.contact-form h2 {
+  text-align: center;
   color: #fff;
-  text-decoration: none;
-  border-radius: 5px;
-  transition: background-color 0.3s ease;
-  margin-right: 10px;
 }
 
-.view-more-btn:hover,
-.add-to-cart-btn:hover {
-  background-color: #004080;
+.contact-form .form-group {
+  margin-bottom: 1rem;
+}
+
+.contact-form label {
+  color: #fff;
+}
+
+.contact-form input,
+.contact-form textarea {
+  width: 100%;
+  padding: 0.5rem;
+  background-color: transparent;
+  border: none;
+  border-left: 3px solid transparent;
+  border-bottom: 1px solid #fff;
+  color: #fff;
+  margin-top: 5px;
+}
+
+.contact-form textarea {
+  resize: vertical;
+}
+
+.contact-form .send-message {
+  margin-top: 20px;
+  text-align: center;
+}
+
+.contact-form button {
+  padding: 0.75rem 1rem;
+  border: none;
+  background-color: #007bff;
+  color: #fff;
+  cursor: pointer;
+}
+
+.contact-form button:hover {
+  background-color: #0056b3;
+}
+
+.send-message {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.contact-icons {
+  display: flex;
+  align-items: center;
+}
+
+.contact-icons svg {
+  margin-right: 10px; /* Adjust as needed */
+}
+
+.contact-info {
+  color: #fff;
 }
 </style>
